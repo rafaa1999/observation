076c5d880bb2fd8a8611fe7791594fa374fc7cd4
rafaa1999 <raferhouma10@gmail.com>
2024-07-05 11:42:08 +0100

    Edit configuration

--- src/main/java/com/rafaa/OrderController.java
+++ src/main/java/com/rafaa/OrderController.java
@@ -1,20 +1,26 @@
 package com.rafaa;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.time.ZonedDateTime;
-
 @RestController
 @RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderRepository orderRepository;
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 
     @GetMapping("/{id}")
     public Order findById(@PathVariable Long id){
-       return new Order(id,1L, ZonedDateTime.now(), BigDecimal.TEN);
+        log.info("OrderController.findById(id)");
+        return this.orderRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid id: %d".formatted(id)));
     }
 
 }
